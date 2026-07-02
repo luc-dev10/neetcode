48f522b90ea3765a01133d648b39ca4bf4e229ff
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-02 22:41:15 +0000

    Stack - Evaluate Reverse Polish Notation question

---
+++ 2_stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,16 @@
+/**
+ * https://neetcode.io/problems/evaluate-reverse-polish-notation/question?list=neetcode150
+ * 
+ * You are given an array of strings tokens that represents a valid arithmetic expression in Reverse Polish Notation.
+ * Return the integer that represents the evaluation of the expression.
+ * The operands may be integers or the results of other operations
+ * The operators include '+', '-', '*', and '/'
+ * Assume that division between integers always truncates toward zero.
+ * 
+ * Constraints:
+ * 1 <= tokens.length <= 1000
+ * tokens[i] is "+", "-", "*", or "/", or a string representing an integer in the range [-200, 200].
+ */
+public class Evaluate ReversePolishNotation {
+
+}
