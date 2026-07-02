46b46fbf6815396b0956a680d9d90177d6bb6e52
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-02 23:16:40 +0000

    Binary Search - Search In Rotated Sorted Array solution

--- 3_binary_search/SearchInRotatedSortedArray.java
+++ 3_binary_search/SearchInRotatedSortedArray.java
@@ -16,4 +16,24 @@
  * All values of nums are unique
  * nums is an ascending array that is possibly rotated.
  */
-public class SearchInRotatedSortedArray {}
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = (r - l) / 2 + l;
+            if (nums[m] == target) return m;
+
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target < nums[m]) r = m - 1;
+                else l = m + 1;
+            } else {
+                if (nums[m] < target && target <= nums[r]) l = m + 1;
+                else r = m - 1;
+            }
+        }
+
+        return - 1;
+    }
+}
