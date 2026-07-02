34c4609ccd744935c9e1469561e0b0ff4c5e89f0
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-02 22:39:08 +0000

    Stack - Daily Temperatures question

---
+++ 2_stack/DailyTemperatures.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * https://neetcode.io/problems/daily-temperatures/question?list=neetcode150
+ * 
+ * You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
+ * Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.
+ * 
+ * Constraints:
+ * 1 <= temperatures.length <= 1000.
+ * 1 <= temperatures[i] <= 100
+ */
+public class DailyTemperatures {
+
+}
