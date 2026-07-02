b8993dd3c2ffd5eaf7308e0cd2f3e8605d97de46
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-02 22:45:40 +0000

    Stack - Min Stack question

---
+++ 2_stack/MinStack.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * https://neetcode.io/problems/minimum-stack/question?list=neetcode150
+ * 
+ * Design a stack class that supports the push, pop, top, and getMin operations.
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
+ * Each function should run in O(1) O(1) time.
+ * 
+ * Constraints:
+ * -2^31 <= val <= 2^31 - 1.
+ * pop, top and getMin will always be called on non-empty stacks.
+ */
+public class MinStack {
+
+}
