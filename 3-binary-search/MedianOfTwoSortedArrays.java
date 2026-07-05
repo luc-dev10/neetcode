70122b8bff34decdf6a06e595d106cba8e41beee
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-05 21:07:43 +0000

    Binary Search - Median Of Two Sorted Arrays question

---
+++ 3-binary-search/MedianOfTwoSortedArrays.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * https://neetcode.io/problems/median-of-two-sorted-arrays/question
+ * 
+ * You are given two integer arrays nums1 and nums2 of size m and n respectively, where each is sorted in ascending order. Return the median value among all elements of the two arrays.
+ * Your solution must run in O(log(m+n)) O(log(m+n)) time.
+ * 
+ * Constraints:
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * -10^6 <= nums1[i], nums2[i] <= 10^6
+ */
+public class MedianOfTwoSortedArrays {}
