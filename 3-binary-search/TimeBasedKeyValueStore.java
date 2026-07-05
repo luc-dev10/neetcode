87f89fc9f2f3ca2b1130ac8285181915ad42bd17
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-05 20:55:48 +0000

    Binary Search - Time Based Key-Value Store solution

--- 3-binary-search/TimeBasedKeyValueStore.java
+++ 3-binary-search/TimeBasedKeyValueStore.java
@@ -14,5 +14,54 @@
  * 0 <= timestamp <= 10^7
  * All the timestamps of set are strictly increasing.
  */
-public class TimeBasedKeyValueStore {
+class TimeBasedKeyValueStore {
+    Map<String, List<Pair<Integer, String>>> hm;
+
+    public TimTimeBasedKeyValueStoreeMap() {
+        this.hm = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        List<Pair<Integer, String>> ls = this.hm.computeIfAbsent(key, k -> new ArrayList<>());
+        ls.add(new Pair<>(timestamp, value));
+        this.hm.put(key, ls);
+    }
+    
+    public String get(String key, int timestamp) {
+        List<Pair<Integer, String>> ls = this.hm.getOrDefault(key, new ArrayList<>());
+        int l = 0;
+        int r = ls.size() - 1;
+        String res = "";
+        
+        while (l <= r) {
+            int m = (r - l) / 2 + l;
+            Pair<Integer, String> p = ls.get(m);
+            if (timestamp >= p.getKey()) {
+                res = ls.get(m).getValue();
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return res;
+    }
+}
+
+class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
 }
