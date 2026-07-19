bc2ecbab72bc726bb05a50fe2b3f9c43d0b9884f
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-19 18:37:08 +0000

    Linked List - Merge Two Sorted Linked Lists question

---
+++ 5-linked-list/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ *  https://neetcode.io/problems/merge-two-sorted-linked-lists/question?list=neetcode150
+ * 
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+ * The new list should be made up of nodes from list1 and list2.
+ * 
+ * Constraints:
+ * 0 <= The length of the each list <= 100.
+ * -100 <= Node.val <= 100
+ * 
+ */
+
+public class MergeTwoSortedLinkedLists {
+}
