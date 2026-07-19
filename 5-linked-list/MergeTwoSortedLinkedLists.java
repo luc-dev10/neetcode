f7f3e6aab8bbab9ceda3b5272f896243ffd012b6
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-19 20:03:50 +0000

    Linked List - Merge Two Sorted Linked Lists solution

--- 5-linked-list/MergeTwoSortedLinkedLists.java
+++ 5-linked-list/MergeTwoSortedLinkedLists.java
@@ -13,4 +13,36 @@
  */
 
 public class MergeTwoSortedLinkedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode lp = list1;
+        ListNode rp = list2;
+        ListNode current = dummy;
+
+        while (lp != null && rp != null) {
+            if (lp.val <= rp.val) {
+                current.next = lp;
+                lp = lp.next;
+            } else {
+                current.next = rp;
+                rp = rp.next;
+            }
+
+            current = current.next;
+        }
+
+        current.next = (lp != null) ? lp : rp;
+        return dummy.next;
+    }   
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
