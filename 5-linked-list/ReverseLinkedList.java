27c0ec5ffb08fb8ddebf3443f043d0ec86058722
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-19 18:30:07 +0000

    Linked List - Reverse Linked List question

---
+++ 5-linked-list/ReverseLinkedList.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * https://neetcode.io/problems/reverse-a-linked-list/question?list=neetcode150
+ *
+ * Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+ * 
+ * 
+ * Constraints:
+ * 0 <= The length of the list <= 1000.
+ * -1000 <= Node.val <= 1000
+ **/
+
+public class ReverseLinkedList {
+}
