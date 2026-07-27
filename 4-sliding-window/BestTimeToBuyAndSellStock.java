8713a479fc4f01ed51af8b03fb42916598a4168d
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-27 23:17:00 +0000

    Sliding Window - Best Time to Buy and Sell Stock question

---
+++ 4-sliding-window/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ * https://neetcode.io/problems/buy-and-sell-crypto/question?list=neetcode150 
+ * 
+ * You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+ * You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+ * Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+ *
+ * Constraints:
+ * 1 <= prices.length <= 100
+ *  <= prices[i] <= 100
+ * 
+ */ 
+public class BestTimeToBuyAndSellStock() {
+
+}
