adc635a9ee7fa7c0b81df9efacdd2e5fe466c258
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-07-29 22:48:47 +0000

    Linked LIst - Copy Linked List with Random Pointer question

---
+++ 5-linked-list/CopyLinkedListWithRandomPointer.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * https://neetcode.io/problems/copy-linked-list-with-random-pointer/question?list=neetcode150
+ * 
+ * You are given the head of a linked list of length n. Unlike a singly linked list, each node contains an additional pointer random, which may point to any node in the list, or null.
+ * Create a deep copy of the list.
+ * The deep copy should consist of exactly n new nodes, each including:
+ * The original value val of the copied node
+ * A next pointer to the new node corresponding to the next pointer of the original nod
+ * A random pointer to the new node corresponding to the random pointer of the original node
+ * Note: None of the pointers in the new list should point to nodes in the original list.
+ * 
+ * Return the head of the copied linked list.
+ * 
+ * In the examples, the linked list is represented as a list of n nodes. Each node is represented as a pair of [val, random_index] where random_index is the index of the node (0-indexed) that the random pointer points to, or null if it does not point to any node.
+ * 
+ * Constraints:
+ * 0 <= n <= 100
+ * -100 <= Node.val <= 100
+ * Node values are not guaranteed to be unique.
+ * random is null or is pointing to some node in the linked list.
+ * 
+ */
+public class CopyLinkedListWithRandomPointer() {
+
+}
