8b370eb86e79888e753bae439184e714fff02605
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-05 01:34:54 +0000

    Linked List - Find the Duplicate Number question

---
+++ 5-linked-list/FindTheDuplicateNumber.java
@@ -0,0 +1,16 @@
+/**
+ * https://neetcode.io/problems/find-duplicate-integer/question?list=neetcode150
+ * 
+ * You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
+ * There is exactly one repeated integer in nums, and every other integer appears at most once.
+ * Return the repeated integer.
+ * 
+ * Follow-up: Can you solve the problem without modifying the array nums and using 
+ * O(1)O(1) extra space?
+ * 
+ * Constraints:
+ * 1 <= n <= 10,000
+ * nums.length == n + 1
+ * 1 <= nums[i] <= n
+ */
+public class FindTheDuplicateNumber {}
