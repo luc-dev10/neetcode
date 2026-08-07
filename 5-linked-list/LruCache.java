28686a795dc7557aee7b1664d685d51f7677b114
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-07 03:17:13 +0000

    Linked List - LRU Cache question

---
+++ 5-linked-list/LruCache.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * https://neetcode.io/problems/lru-cache/question?list=neetcode150
+ * 
+ * Implement the Least Recently Used (LRU) cache class LRUCache. The class should support the following operations
+ * LRUCache(int capacity) Initialize the LRU cache of size capacity.
+ * int get(int key) Return the value corresponding to the key if the key exists, otherwise return -1.
+ * void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the introduction of the new pair causes the cache to exceed its capacity, remove the least recently used key.
+ * 
+ * A key is considered used if a get or a put operation is called on it.
+ * 
+ * Ensure that get and put each run in O(1) O(1) average time complexity.
+ * 
+ * Constraints:
+ * 1 <= capacity <= 3000
+ * 0 <= key <= 10^4
+ * 0 <= value <= 10^5
+ * At most 2 * 10^5 calls will be made to get and put.
+ */
+public class LruCache {
+    public LruCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+}
