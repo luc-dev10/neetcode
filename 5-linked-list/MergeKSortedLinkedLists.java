95db766308008d2dfa3517bfbaa3c48e97252a9b
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-13 03:03:49 +0000

    Linked List - Merge K Sorted Linked Lists question

---
+++ 5-linked-list/MergeKSortedLinkedLists.java
@@ -0,0 +1,17 @@
+/**
+ * https://neetcode.io/problems/merge-k-sorted-linked-lists/question?list=neetcode150
+ * 
+ * You are given an array of k linked lists lists, where each list is sorted in ascending order.
+ * 
+ * Return the sorted linked list that is the result of merging all of the individual linked lists.
+ * 
+ * Constraints:
+ * 0 <= lists.length <= 10000
+ * 0 <= lists[i].length <= 500
+ * -10000 <= lists[i][j] <= 10000
+ * lists[i] is sorted in ascending order.
+ * The sum of lists[i].length will not exceed 10000.
+ */
+public class MergeKSortedLinkedLists {
+
+}
