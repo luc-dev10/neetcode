40fcaca53bc8615db7807ab7f292fa8c0c444a69
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-16 17:25:40 +0000

    Linked List - Reverse Nodes in K-Group question

---
+++ 5-linked-list/ReverseNodesInKGroup.java
@@ -0,0 +1,16 @@
+/**
+ * https://neetcode.io/problems/reverse-nodes-in-k-group/question?list=neetcode150
+ * 
+ * You are given the head of a singly linked list head and a positive integer k.
+ * You must reverse the first k nodes in the linked list, and then reverse the next k nodes, and so on. If there are fewer than k nodes left, leave the nodes as they are.
+ * Return the modified list after reversing the nodes in each group of k.
+ * You are only allowed to modify the nodes' next pointers, not the values of the nodes.
+ * 
+ * Constraints: 
+ * The length of the linked list is n
+ * 1 <= k <= n <= 5000
+ * 0 <= Node.val <= 100
+ */
+public class ReverseNodesInKGroup {
+
+}
