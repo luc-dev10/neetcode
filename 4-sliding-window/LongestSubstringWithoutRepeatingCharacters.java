9ccd1c27b6e24b6ea4e83961c2bc23e39719ca04
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-19 03:02:12 +0000

    Sliding Window - Longest Substring Without Repeating Characters question

---
+++ 4-sliding-window/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * https://neetcode.io/problems/longest-substring-without-duplicates/question?list=neetcode150
+ * 
+ * Given a string s, find the length of the longest substring without duplicate characters.
+ * A substring is a contiguous sequence of characters within a string.
+ * 
+ * Constraints:
+ * 0 <= s.length <= 50,000
+ * s may consist of printable ASCII characters.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {}
