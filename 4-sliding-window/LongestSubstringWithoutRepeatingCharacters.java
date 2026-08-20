5a784dc35ba83d782b13a58d9389f8edb9cdbc61
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-20 23:34:49 +0000

    Sliding Window - Longest Substring Without Repeating Characters solution

--- 4-sliding-window/LongestSubstringWithoutRepeatingCharacters.java
+++ 4-sliding-window/LongestSubstringWithoutRepeatingCharacters.java
@@ -9,4 +9,19 @@
  * 0 <= s.length <= 50,000
  * s may consist of printable ASCII characters.
  */
-public class LongestSubstringWithoutRepeatingCharacters {}
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> hm = new HashMap<>();
+        int l = 0;
+        int r = 0;
+        int t =  0;
+        while (r < s.length()) {
+            if (hm.containsKey(s.charAt(r))) l = Math.max(l, hm.get(s.charAt(r)) + 1);
+            t = Math.max(t, r - l + 1);
+            hm.put(s.charAt(r), r);
+            r++;
+        }
+
+        return t;
+    }
+}
