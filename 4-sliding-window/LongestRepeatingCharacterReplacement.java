e15ae00a0d96c39b80d70c6934db303944799f8b
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-22 02:49:24 +0000

    Sliding Window - Longest Repeating Character Replacement question

---
+++ 4-sliding-window/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,19 @@
+/**
+ * https://neetcode.io/problems/longest-repeating-substring-with-replacement/question?list=neetcode150
+ *
+ * You are given a string s consisting of only uppercase english characters and an integer k.
+ * 
+ * You can choose up to k characters of the string and replace them with any other uppercase English character.
+ * 
+ * After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
+ * 
+ * Constraints:
+ * 
+ * 1 <= s.length <= 100,000
+ * 0 <= k <= s.length
+ * s consists of only uppercase english characters.
+ * 
+ * */
+public class LongestRepeatingCharacterReplacement {
+    
+}
