fc0f0bfa20de5638f2166fdeb55edebb4d73e59a
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-23 19:03:25 +0000

    Sliding Window - Permutation in String question

---
+++ 4-sliding-window/PermutationInString.java
@@ -0,0 +1,11 @@
+/**
+ * https://neetcode.io/problems/permutation-string/question?list=neetcode150
+ * 
+ * You are given two strings s1 and s2.
+ * Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+ * Both strings only contain lowercase letters.
+ * 
+ * Constraints:
+ * 1 <= s1.length, s2.length <= 10000
+ */
+public class PermutationInString {}
