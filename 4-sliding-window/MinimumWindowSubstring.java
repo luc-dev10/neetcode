e5200bf21ca465b02db72d2943d259197c9d3407
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-25 19:55:31 +0000

    Sliding Window - Minimum Window Substring question

---
+++ 4-sliding-window/MinimumWindowSubstring.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * https://neetcode.io/problems/minimum-window-with-characters/question?list=neetcode150
+ * 
+ * Given two strings s and t, return the shortest substring of s such that every character in t, 
+ * including duplicates, is present in the substring. If such a substring does not exist, return an empty string "".
+ * You may assume that the correct output is always unique.
+ * 
+ * Constraints:
+ * 1 <= s.length <= 100,000
+ * 1 <= t.length <= 100,000
+ * s and t consist of uppercase and lowercase English letters.
+ */
+public class MinimumWindowSubstring {}
