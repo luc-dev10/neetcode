90c13822e26cddc911f22c6636ff32a46bfe5887
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-29 20:02:27 -0400

    Sliding Window - Minimum Window Substring solution

--- 4-sliding-window/MinimumWindowSubstring.java
+++ 4-sliding-window/MinimumWindowSubstring.java
@@ -11,4 +11,39 @@
  * 1 <= t.length <= 100,000
  * s and t consist of uppercase and lowercase English letters.
  */
-public class MinimumWindowSubstring {}
+public class MinimumWindowSubstring {
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> reqHm = new HashMap<>();
+        Map<Character, Integer> windowHm = new HashMap<>();
+        for (char c : t.toCharArray()) reqHm.put(c, reqHm.getOrDefault(c, 0) + 1);
+
+        int wc = 0;
+        int rc = reqHm.size();
+        int l = 0;
+        int rl = 0;
+        int rr = 0;
+        int tl = Integer.MAX_VALUE;
+
+        for (int r = 0; r < s.length(); r++) {
+            char k = s.charAt(r);
+            windowHm.put(k, windowHm.getOrDefault(k, 0) + 1);
+            if (reqHm.containsKey(k) && windowHm.get(k).equals(reqHm.get(k))) wc++;
+
+            while (wc == rc) {
+                int w = r - l + 1;
+                if (w < tl) {
+                    tl = w;
+                    rl = l;
+                    rr = r;
+                }
+
+                k = s.charAt(l);
+                if (reqHm.containsKey(k) && windowHm.get(k).equals(reqHm.get(k))) wc--;
+                windowHm.put(k, windowHm.getOrDefault(k, 0)  - 1);
+                l++;
+            }
+        }
+
+        return tl == Integer.MAX_VALUE ? "" : s.substring(rl, rr + 1);
+    }
+}
