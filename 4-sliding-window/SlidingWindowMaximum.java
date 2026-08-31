62ce322b05c886f854d363bb4b9efaa1aa91bcfc
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-08-30 21:00:53 -0400

    Sliding Window - Sliding Window Maximum question

---
+++ 4-sliding-window/SlidingWindowMaximum.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * https://neetcode.io/problems/sliding-window-maximum/question?list=neetcode150
+ * 
+ * You are given an array of integers nums and an integer k. There is a sliding window of size k that starts at the left edge of the array. The window slides one position to the right until it reaches the right edge of the array.
+ * Return a list that contains the maximum element in the window at each step.
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 100,000
+ * -10,000 <= nums[i] <= 10,000
+ * 1 <= k <= nums.length
+ */
+public class SlidingWindowMaximum {}
